ab3a8f98d7df6e39de6caa2bbf4e25ef45b019d8
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2021-01-28 14:50:08 +0530

    updated resume

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -17,7 +17,7 @@ function Navigate(props) {
 <a className="nav-link" onClick={()=>{changePage("Projects")}}>Projects</a>
     </div>
     <div>
-<a className="nav-link" target="_blank" href="https://drive.google.com/file/d/1qsoYIsrqUj1G68-EB_4mOHJqQtDJlq2N/view?usp=sharing">Resume</a>
+<a className="nav-link" target="_blank" href="https://drive.google.com/file/d/1T2-NfaPRh7JXPj3unCOhefE6gfo3U5lH/view?usp=sharing">Resume</a>
     </div>
     </div>
   );
