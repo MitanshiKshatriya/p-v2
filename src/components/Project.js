e524170af15c3396ce52accfe27e488f70304c91
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2021-01-30 14:34:35 +0530

    added links

--- src/components/Project.js
+++ src/components/Project.js
@@ -10,7 +10,7 @@ function Projects() {
     <h3 className="work-heading">Projects</h3> 
     
 <h6>Stadium Tour Management System — Python,SQLite 
-<em>{" "}<u><a href="#">link</a></u></em>
+<em>{" "}<u><a href="https://github.com/MitanshiKshatriya/Stadium-tour-management-system" target="_blank">link</a></u></em>
 </h6>
 
 <p className="work-desc">- A python and sqlite based project for easing the task of a stadium tour
@@ -18,14 +18,14 @@ manager. Uses various sql query statements. Applies genetic algorithms
 to find out the best possible route for the tour.</p>
 
 <h6>IEEE WIE VIT website — HTML,CSS,JavaScript
-<em>{" "}<u><a href="#">link</a></u></em>
+<em>{" "}<u><a href="https://ieeewievit.org/" target="_blank">link</a></u></em>
 </h6>
 
 <p className="work-desc">- Built an informative page for IEEE WIE VIT with animation using vanilla
 JavaScript</p>
 
 <h6>Mood Player — Python,SpotifyAPI
-<em>{" "}<u><a href="#">link</a></u></em>
+<em>{" "}<u><a href="https://github.com/MitanshiKshatriya/mood-player" target="_blank">link</a></u></em>
 </h6>
 
 <p className="work-desc">- Built a mood player using CNN, OpenCV and SpotifyAPI. Used CNN to
@@ -34,7 +34,7 @@ the mood detected.
 </p>
 
 <h6>Encryption/Decryption using CRT — Python
-<em>{" "}<u><a href="#">link</a></u></em>
+<em>{" "}<u><a href="https://github.com/MitanshiKshatriya/encryption-decryption-using-CRT" target="_blank">link</a></u></em>
 </h6>
 
 <p className="work-desc">- Implemented a research paper to encrypt and decrypt using only Chinese Remainder Theorem (CRT) and
@@ -42,7 +42,7 @@ RSA with CRT.
 </p>
 
 <h6>Taylor Swift Api — NodeJS
-<em>{" "}<u><a href="#">link</a></u></em>
+<em>{" "}<u><a href="https://protected-plains-05402.herokuapp.com/" target="_blank">link</a></u></em>
 </h6>
 
 <p className="work-desc">- A random taylor swift lyric generator made using NodeJS and deployed on Heroku
